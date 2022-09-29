0229c7d44bdc31238a5cecb3c0ea351b23ac477a
daviana putra <106154800+motebaya@users.noreply.github.com>
2022-09-29 21:55:33 +0700

    update cli argument

--- index.js
+++ index.js
@@ -1,113 +1,76 @@
 #!/usr/bin/node
+/*
+just for learn about promise and asynchronous function
+last update: new Date() => {
+    Thu Sep 29 2022 21:52:44 GMT+0700 (Western Indonesia Time)
+}
+*/
+const { get_data, get_video_id } = require("./lib/Tiktok.js");
+const { download } = require("./lib/Downloader.js");
+const { ArgumentParser, RawTextHelpFormatter } = require("argparse");
 
-const { get_data, get_video_id } = require("./lib/Tiktok.js")
-const { downloader } = require("./lib/Downloader.js")
-const { ArgumentParser } = require("argparse")
+var [
+    reset,
+    green
+] = [
+    '\x1b[0m',
+    '\x1b[32m'
+]
 
-async function Main(config, video_url){
-    await get_data(video_url)
-    .then((res) => {
-        console.log(
-            `
- [@] author: ${res.author.name} (${res.author.username})
- [@] biography: ${res.author.biography}
- [@] desc: ${res.desc}
- [@] views: ${res.stats.total_views}
- [@] config: ${config.join(", ")}
-            `
-        )
-        if (config.includes('a')){
-            console.log(
-                ` [log] downlaoding Avatar..`
-            )
-            downloader(
-                res.author.avatar, `${res.author.name}-${res.author.username}.png`
-            )
-        }
+const debug = (string) => {
+    console.log(
+        ` ${reset}[${green}${new Date().toString().split(/\s/)[4]}${reset}] ${string}`)
+}
 
-        if (config.includes('m')){
-            console.log(" [log] downloading Music..")
-            downloader(
-                res.music.url, `${res.music.title}.mp3`
-            )
-            console.log(" [log] downlaoding music cover..")
-            downloader(
-                res.music.cover, `${res.music.title}.jpg`
-            )
-        }
+const banner = `\n${reset}     ┌┬┐┬┬┌─┌┬┐┌─┐┬┌─\n      │ │├┴┐ │ │ │├┴┐\n      ┴ ┴┴ ┴ ┴ └─┘┴ ┴\n Tiktok dl with aweme APi\n  \u00A9 github.com/motebaya\n`;
 
-        if (config.includes('v')){
-            console.log(" [log] downloading video..")
-            filename = res.desc.split("#")[0].trim()
-            if (filename.startsWith('#')){
-                filename = res.media.match(/\/([a-z0-9]+)\//i)[1]
+const main = async (url) => {
+    const parser = new ArgumentParser({
+        description: banner,
+        formatter_class: RawTextHelpFormatter
+    })
+    parser.add_argument('-u', '--url', {type: 'str', metavar: '', help: 'url tiktok video'})
+    parser.add_argument('-v', '--video', {action: 'store_true', help: 'get only video (without watermark)'})
+    parser.add_argument('-m', '--music', {action: 'store_true', help: 'get only music video'})
+    parser.add_argument('-a', '--avatar', {action: 'store_true', help: 'get only avatar image'})
+
+    const group = parser.add_argument_group('additional', {
+        description: 'info tools'
+    })
+    group.add_argument('-i', '--info', {action: 'store_true', help: 'show info'})
+    const arg = parser.parse_args()
+    if (arg.url){
+        let video_id = await get_video_id(arg.url)
+        await get_data(video_id).then(async(res) => {
+            console.log(banner)
+            debug(`author: ${res.author.name} (${res.author.username})`);
+            debug(`description: ${res.desc}`)
+            debug(`total views: ${res.stats.total_views}`);
+            if (arg.video){
+                var filename = `${res.desc.split("#")[0].trim()}.mp4`;
+                if (filename.startsWith('#')) filename =`${res.media.match(/\/([a-z0-9]+)\//i)[1]}.mp4`;
+                debug(`downloading video ... ${filename}`)
+                await download(res.media, filename).then((resp) => {console.log()})
             }
-            downloader(
-                res.media, `${filename}.mp4`
-            )
-        }
-        // console.log(
-        //     " [ok] done"
-        // )
-    }).catch((err) => {
-        console.error(err)
-    })
+            if (arg.music){
+                let title = res.music.title
+                debug(`downloading music file ... ${title}.mp3`)
+                await download(res.music.url, `${title}.mp3`).then((resp) => {console.log()})
+                debug(`downloading music cover.. ${title}.jpg`)
+                await download(res.music.cover, `${title}.jpg`).then((resp) => {console.log()})
+            }
+            if (arg.avatar){
+                let filename = `${res.author.username}.jpg`
+                debug(`downloading avatar images.. ${filename}`)
+                await download(res.author.avatar, filename).then(() => {console.log()})
+
+            }
+            // console.log(res)
+        })
+        // console.log(video_id)
+    } else {
+        parser.print_help()
+    }
 }
 
-const _parse = new ArgumentParser({
-    description: "A simple tiktok downloader\n @ motebaya 2022 "
-})
-
-_parse.add_argument("-u", "--url", {
-    metavar: '',
-    type: 'str',
-    help: 'url tiktok video'
-})
-
-_parse.add_argument("-a", "--avatar", {
-    action: 'store_true',
-    help: 'only get avatar'
-})
-
-_parse.add_argument("-m", "--music", {
-    action: 'store_true',
-    help: 'url get music'
-})
-
-_parse.add_argument("-v", "--video", {
-    action: 'store_true',
-    help: 'only get tiktok video (without watermark)'
-})
-
-_parse.add_argument("-g", "--grab", {
-    action: 'store_true',
-    help: 'download all media, include music, video .etc'
-})
-
-_more = _parse.add_argument_group("additional", {
-    description: "if u dont know to usage, just use this command"
-})
-_more.add_argument("-e", "--example", {
-    action: 'store_true',
-    help: 'show example for usage '
-})
-
-arg = _parse.parse_args()
-if (arg.url){
-    let config = []
-    if (arg.avatar) config.push('a')
-    if (arg.music) config.push('m')
-    if (arg.video) config.push('v')
-    if (!config.length) config.push(...['a','m', 'v'])
-    get_video_id(arg.url).then((video_id) => {
-        Main(config, video_id)
-    }).catch((err) => {
-        throw Error(err)
-    })
-} else {
-    _parse.print_help()
-}
-
-// console.log(arg)
-// console.log(downloader)
-// downloader("https://images3.alphacoders.com/127/1273326.jpg", "rebeca.png")
+main().catch(console.error)
